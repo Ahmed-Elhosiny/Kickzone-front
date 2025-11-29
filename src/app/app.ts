1cc70e48605ccc261fac5c36e9eb1834318512ce
MohamedMagdy312 <mohamedmagdysoliman31292@gmail.com>
2025-11-29 18:29:28 +0200

    1st HomePage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBar } from './Component/nav-bar/nav-bar';
+import { Header } from './Component/header/header';
+import { Body } from "./Component/body/body";
+import { Footer } from "./Component/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NavBar, Header, Body, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('KickZone_Project');
