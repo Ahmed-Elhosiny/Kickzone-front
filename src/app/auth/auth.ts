455f6b905123aff5ab9ef6eb7b4755cdf276435a
Osama Mishmish <osama.mishmish16@gmail.com>
2025-11-30 16:54:52 +0200

    enable httpClient

--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Auth {
-  
+export class AuthService {
+  private apiUrl = 'https://localhost:7263/api/Auth/register';
+
+  constructor(private http: HttpClient) {}
+
+  register(userData: any): Observable<any> {
+    return this.http.post(this.apiUrl, userData);
+  }
 }
+
