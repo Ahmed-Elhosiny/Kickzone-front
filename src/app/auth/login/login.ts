85b1811a24023854a841d52b38a0683078120faa
Osama Mishmish <osama.mishmish16@gmail.com>
2025-12-02 09:26:27 +0200

    create login form and update login.ts

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,11 +1,54 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../auth';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+   loginForm: FormGroup;
+  loading = false;
+  serverError: string | null = null;
+  showPassword = false;
 
+  constructor(private fb: FormBuilder, private auth: AuthService, private router: Router) {
+    this.loginForm = this.fb.group({
+      emailOrUserName: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    this.serverError = null;
+
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+
+    const { emailOrUserName, password } = this.loginForm.value;
+
+    this.auth.login({ emailOrUserName, password }).subscribe({
+      next: (res) => {
+        console.log('Login successful:', res);
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        this.loading = false;
+        console.error('Login failed:', err);
+        this.serverError = err?.error?.message || 'Login failed';
+      },
+    });
+  }
 }
+
