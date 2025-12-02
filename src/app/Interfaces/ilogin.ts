b6149eb7f5212142636f12f49365118ce5b8f4b3
Osama Mishmish <osama.mishmish16@gmail.com>
2025-12-02 08:50:48 +0200

    prepare service and interface for login

--- src/app/Interfaces/ilogin.ts
+++ src/app/Interfaces/ilogin.ts
@@ -1,2 +1,10 @@
-export interface Ilogin {
+export interface IloginRequest {
+  emailOrUserName: string;
+  password: string;
+}
+
+export interface IloginResponse{
+  token: string;
+  refreshToken?: string;
+  expiresIn?: number;
 }
--- src/app/auth/auth.ts
+++ src/app/auth/auth.ts
@@ -1,18 +1,58 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable,throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { IRegister } from '../Interfaces/iregister';
+import { IloginRequest, IloginResponse } from '../Interfaces/ilogin';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'https://localhost:7263/api/Auth/register';
 
+  //URLs for authentication
+  private regUrl = 'https://localhost:7263/api/Auth/register';
+  private loginUrl = 'https://localhost:7263/api/Auth/login';
+
+  private readonly tokenKey = 'access_token';
   constructor(private http: HttpClient) {}
-
+  //registration method
   register(userData: IRegister): Observable<any> {
-    return this.http.post(this.apiUrl, userData);
+    return this.http.post(this.regUrl, userData);
+  }
+
+  //login method
+  login(payload: IloginRequest): Observable<IloginResponse> {
+    return this.http.post<IloginResponse>(`${this.loginUrl}/login`, payload).pipe(
+      tap(res => {
+        if (res?.token) {
+          localStorage.setItem(this.tokenKey, res.token);
+        }
+        if (res?.refreshToken) {
+          localStorage.setItem('refresh_token', res.refreshToken);
+        }
+      }),
+      catchError(err => {
+
+        return throwError(() => err);
+      })
+    );
+  }
+
+
+  //logout method
+  logout(): void {
+    localStorage.removeItem(this.tokenKey);
+    localStorage.removeItem('refresh_token');
+  }
+
+  //get token method
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+//authentication status
+  isAuthenticated(): boolean {
+    return !!this.getToken();
   }
 }
 
