ba13b85b846b68da171aa4b768ced31e1ab855c4
Osama Mishmish <osama.mishmish16@gmail.com>
2025-12-12 03:50:47 +0200

    add ts and html content for add-item-dialog component

--- src/app/dialogs/add-item/add-item.ts
+++ src/app/dialogs/add-item/add-item.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-add-item',
-  imports: [],
+  selector: 'app-add-item-dialog',
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './add-item.html',
-  styleUrl: './add-item.css',
 })
-export class AddItem {
+export class AddItemDialogComponent {
+  name = new FormControl('', [Validators.required]);
 
+  constructor(
+    public dialogRef: MatDialogRef<AddItemDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string }
+  ) {}
+
+  submit() {
+    if (this.name.invalid) return;
+    this.dialogRef.close(this.name.value);
+  }
 }
