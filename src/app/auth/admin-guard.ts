d6a01564445f112753c16a6e0db943332023fe4a
Ahmed Elhosiny <ahmedelhosiny842@gmail.com>
2025-12-13 23:09:45 +0200

    refactor: update src/app/auth/admin-guard.ts

--- src/app/auth/admin-guard.ts
+++ src/app/auth/admin-guard.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './auth';
 
@@ -6,8 +6,8 @@ import { AuthService } from './auth';
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
-
-  constructor(private auth: AuthService, private router: Router) {}
+  private auth = inject(AuthService);
+  private router = inject(Router);
 
   canActivate(): boolean {
     const role = this.auth.getUserRole();
