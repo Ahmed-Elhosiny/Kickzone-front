d7aa86ff873299149e994bdc757a84f692a406dd
Osama Mishmish <osama.mishmish16@gmail.com>
2025-12-16 18:41:53 +0200

    update user-Bookings service

--- src/app/services/user-bookings.ts
+++ src/app/services/user-bookings.ts
@@ -1,30 +1,42 @@
 import { Injectable, signal } from '@angular/core';
 import { ReservationService } from './Reservation/reservation';
-import { IReservation } from '../Model/IReservation/ireservation';
+import { IGetReservationDto } from '../Model/IReservation/ireservation-dto';
 import { AuthService } from '../auth/auth';
+import { ReservationStatus } from '../Model/IReservation/ireservation-dto';
 
 @Injectable({ providedIn: 'root' })
 export class UserBookingService {
-  reservations = signal<IReservation[]>([]);
-  loading = signal(true);
+  reservations = signal<IGetReservationDto[]>([]);
+  loading = signal(false);
 
-  constructor(private reservationService: ReservationService , private authService: AuthService) {}
+  constructor(
+    private reservationService: ReservationService,
+    private authService: AuthService
+  ) {}
 
   loadReservations() {
     this.loading.set(true);
 
-    this.reservationService.getMyReservations(this.authService.getUserId()).subscribe({
+    const userId = this.authService.getUserId();
+    if (!userId) return;
+
+    this.reservationService.getMyReservations(userId).subscribe({
       next: (res) => {
         this.reservations.set(res);
         this.loading.set(false);
       },
-      error: () => {
-        this.loading.set(false);
-      }
+      error: () => this.loading.set(false),
     });
   }
 
-  paidReservations() {
-    return this.reservations().filter(r => r.amountPaid > 0);
+  /** الحجوزات المدفوعة ولسه جاية */
+  upcomingPaidReservations() {
+    const now = new Date();
+
+    return this.reservations().filter(r =>
+      r.amountPaid > 0 &&
+      r.status === ReservationStatus.Complete &&
+      new Date(r.slotStart) > now
+    );
   }
 }
