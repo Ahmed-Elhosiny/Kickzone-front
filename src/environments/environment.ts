476ccf483ad0c461b6fb4feddbe9dc7cdd847c2c
Ahmed Elhosiny <ahmedelhosiny842@gmail.com>
2025-12-28 01:58:56 +0200

    config: switch API URL to production environment
    - Change apiUrl from localhost:7263 to kickzoneapi.runasp.net
    - Prepare for production deployment
    - Comment out localhost URL for easy switching back during development

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://kickzoneapi.runasp.net/api'
-   apiUrl: 'https://localhost:7263/api'
+  apiUrl: 'https://kickzoneapi.runasp.net/api'
+  //  apiUrl: 'https://localhost:7263/api'
 };
 
